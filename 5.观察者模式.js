2fcb0e846e98fbe0392b02aa8f6d8ced2169b6b9
dezhizhang <1541609448@qq.com>
2022-06-18 15:33:34 +0800

    feat:update

--- 5.观察者模式.js
+++ 5.观察者模式.js
@@ -1,37 +1,3 @@
-# node
-
-### 发布订阅
-```js
-
-const fs = require('fs');
-const path = require('path');
-
-const events = {
-    _array:[],
-    on(callback) {
-        this._array.push(callback);
-    },
-    emit(key,value) {
-        this._array.map(fn => fn(key,value))
-    }
-}
-
-
-events.on((key,value) => {
-    console.log('属性:' + key+ "值" + value);
-});
-
-fs.readFile(path.resolve(__dirname,'./name.txt'),'utf-8',(err,data) => {
-    events.emit('name',data)
-});
-
-fs.readFile(path.resolve(__dirname,'./age.txt'),'utf-8',(err,data) => {
-    events.emit('age',data);
-})
-
-```
-### 观察者模式
-```js
 class Subject {
   constructor(name) {
     this.name = name;
@@ -66,5 +32,3 @@ s.attach(o1);
 s.attach(o2);
 
 s.setState('不开心了')
-
-```
